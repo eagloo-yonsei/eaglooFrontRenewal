61327e5611159e8c49ff373ca7ed93b7db00a019
김진아 <wlsdk7245@gmail.com>
2022-03-07 18:06:08 +0900

    feat(#6_entry): 사용자 설정 방 모달

--- src/app.components/CustomRoomModal/CustomRoomModal.tsx
+++ src/app.components/CustomRoomModal/CustomRoomModal.tsx
@@ -1,11 +1,25 @@
-import React from "react";
-import CustomRoomModalProvider from "./CustomRoomModalProvider";
-import CustomRoomModalContainer from "./CustomRoomModalContainer";
+import React from 'react';
+import CustomRoomModalProvider from './CustomRoomModalProvider';
+import CustomRoomModalContainer from './CustomRoomModalContainer';
+import { useGetUser } from 'app.store/intoAPP/store.intoAPP';
 
-export default function CustomRoomModal() {
-    return (
-        <CustomRoomModalProvider>
-            <CustomRoomModalContainer />
-        </CustomRoomModalProvider>
-    );
+export default function CustomRoomModal({
+  showCustomRoomModal,
+  setShowCustomRoomModal,
+}) {
+  const getUser = useGetUser();
+
+  if (getUser?.isLoading) return null;
+  return (
+    <CustomRoomModalProvider
+      isLoggedIn={getUser?.login}
+      userInfo={getUser?.info}
+      setShowCustomRoomModal={setShowCustomRoomModal}
+    >
+      <CustomRoomModalContainer
+        showCustomRoomModal={showCustomRoomModal}
+        setShowCustomRoomModal={setShowCustomRoomModal}
+      />
+    </CustomRoomModalProvider>
+  );
 }
--- src/app.components/CustomRoomModal/CustomRoomModal__RoomButton.tsx
+++ src/app.components/CustomRoomModal/CustomRoomModal__RoomButton.tsx
@@ -1,60 +1,59 @@
-import React from "react";
-import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserAlt } from "@fortawesome/free-solid-svg-icons";
-import { useCustomRoomModalContext } from "./CustomRoomModalProvider";
-import { CustomRoom } from "../../Constants";
-import fatIcon from "../../Resources/Img/icon-fat.png";
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserAlt } from '@fortawesome/free-solid-svg-icons';
+import { useCustomRoomModalContext } from './CustomRoomModalProvider';
+import { CustomRoom } from 'app.modules/constant/interface';
 
 interface CustomRoomButtonProp {
-    room: CustomRoom;
+  room: CustomRoom;
 }
 
 export default function CustomRoomModalRoomButton({
-    room,
+  room,
 }: CustomRoomButtonProp) {
-    const { selectedRoomId, selectRoom } = useCustomRoomModalContext();
+  const { selectedRoomId, selectRoom } = useCustomRoomModalContext();
 
-    return (
-        <Container
-            roomId={room.id}
-            selectedRoomId={selectedRoomId}
-            onClick={() => {
-                selectRoom(room.id);
-            }}
-        >
-            <RoomIcon src={fatIcon} />
-            <RoomContentContainer>
-                <RoomName>{`${room.roomName}`}</RoomName>
-                <RoomDescription>
-                    {room.roomDescription ? room.roomDescription : ""}
-                </RoomDescription>
-                <RoomState>
-                    <FontAwesomeIcon icon={faUserAlt} />
-                    {`  ${room.seats.length}/14`}
-                </RoomState>
-            </RoomContentContainer>
-        </Container>
-    );
+  return (
+    <Container
+      roomId={room.id}
+      selectedRoomId={selectedRoomId}
+      onClick={() => {
+        selectRoom(room.id);
+      }}
+    >
+      <RoomIcon src="/images/common/icon-fat.png" />
+      <RoomContentContainer>
+        <RoomName>{`${room.roomName}`}</RoomName>
+        <RoomDescription>
+          {room.roomDescription ? room.roomDescription : ''}
+        </RoomDescription>
+        <RoomState>
+          <FontAwesomeIcon icon={faUserAlt} />
+          {`  ${room.seats.length}/14`}
+        </RoomState>
+      </RoomContentContainer>
+    </Container>
+  );
 }
 
 interface ContainerProp {
-    roomId: string;
-    selectedRoomId: string;
+  roomId: string;
+  selectedRoomId: string;
 }
 
 const Container = styled.div<ContainerProp>`
-    display: flex;
-    width: 100%;
-    height: 100px;
-    padding: 5px 20px;
-    :hover {
-        cursor: pointer;
-    }
-    background-color: ${(props) =>
-        props.roomId === props.selectedRoomId
-            ? props.theme.loginMessageGray
-            : "none"};
+  display: flex;
+  width: 100%;
+  height: 100px;
+  padding: 5px 20px;
+  :hover {
+    cursor: pointer;
+  }
+  background-color: ${(props) =>
+    props.roomId === props.selectedRoomId
+      ? props.theme.loginMessageGray
+      : 'none'};
 `;
 
 // const RoomIcon = styled.div`
@@ -67,34 +66,34 @@ const Container = styled.div<ContainerProp>`
 // `;
 
 const RoomIcon = styled.img`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    aspect-ratio: 1;
-    border-radius: 24px;
-    margin-right: 15px;
-    overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  aspect-ratio: 1;
+  border-radius: 24px;
+  margin-right: 15px;
+  overflow: hidden;
 `;
 
 const RoomContentContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    color: ${(props) => props.theme.entryMainBlue};
-    font-family: ${(props) => props.theme.plainBoldTextFont};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: ${(props) => props.theme.entryMainBlue};
+  font-family: ${(props) => props.theme.plainBoldTextFont};
 `;
 
 const RoomName = styled.div`
-    font-size: 19px;
-    padding-bottom: 6px;
+  font-size: 19px;
+  padding-bottom: 6px;
 `;
 
 const RoomDescription = styled.div`
-    font-size: 12px;
-    padding-bottom: 12px;
+  font-size: 12px;
+  padding-bottom: 12px;
 `;
 
 const RoomState = styled.div`
-    font-size: 14px;
+  font-size: 14px;
 `;
