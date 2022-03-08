aedd92b91a27abe219b52775a13b6fb7be2b1e0f
김진아 <wlsdk7245@gmail.com>
2022-03-08 10:23:51 +0900

    fix(#6_entry): 관리자 아닌 경우 user 페이지 접근 막음

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { useGetUser } from 'app.store/intoAPP/store.intoAPP';
 import ScreenAdminUserProvider from '../../app.admin/user/screen/ScreenAdminUserProvider';
 import ScreenAdminUser from '../../app.admin/user/screen/ScreenAdminUser';
+import { useRouter } from 'next/router';
+import Loading from 'app.components/Loading/Loading';
 
 const Page_Home: any = () => {
-  const getUser = useGetUser();
+  const { isLoading, info: userInfo } = useGetUser();
+  const router = useRouter();
 
+  if (isLoading) return <Loading />;
+  if (!userInfo?.isAdmin) router.push('/');
   return (
     <>
       <ScreenAdminUserProvider>
