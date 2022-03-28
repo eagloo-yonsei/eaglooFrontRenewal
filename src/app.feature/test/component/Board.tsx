e519e40720d5b57d24b8d76b852a1ee40ff47797
김진아 <wlsdk7245@gmail.com>
2022-03-28 23:14:16 +0900

    test(develop): 화이트보드 사용성 개선

--- src/app.feature/test/component/Board.tsx
+++ src/app.feature/test/component/Board.tsx
@@ -2,35 +2,14 @@ import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { ReactSketchCanvas } from 'react-sketch-canvas';
 
-const Board = ({ size, color }) => {
-  const [eraseMode, setEraseMode] = useState(false);
-  const [eraserSize, setEraserSize] = useState(5);
-
-  const canvasRef = useRef(null);
+const Board = ({ size, color, canvasRef, eraserSize }) => {
   const styles = {
     border: '0.0625rem solid #9c9c9c',
     borderRadius: '0.25rem',
   };
 
-  const handleEraserSize = (event) => {
-    setEraserSize(event.target.value);
-  };
-
-  const handleEraseMode = () => {
-    canvasRef.current.eraseMode(!eraseMode);
-    setEraseMode(!eraseMode);
-  };
-
   return (
     <StyledWrapper>
-      <select value={size} onChange={handleEraserSize}>
-        {[5, 10, 15, 20, 25, 30, 35].map((item, key) => (
-          <option key={key}>{item}</option>
-        ))}
-      </select>
-      <button onClick={handleEraseMode}>
-        {eraseMode ? '그리기' : '지우개'}
-      </button>
       <ReactSketchCanvas
         ref={canvasRef}
         style={styles}
--- src/app.feature/test/screen/ScreenTestBoard.tsx
+++ src/app.feature/test/screen/ScreenTestBoard.tsx
@@ -1,11 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import Board from 'app.feature/test/component/Board';
 
 const ScreenTestBoard = () => {
+  const [eraseMode, setEraseMode] = useState(false);
+  const [eraserSize, setEraserSize] = useState(5);
   const [color, setColor] = useState('#000000');
   const [size, setSize] = useState(5);
+  const canvasRef = useRef(null);
 
+  const handleEraserSize = (event) => {
+    setEraserSize(event.target.value);
+  };
+
+  const handleEraseMode = () => {
+    canvasRef.current.eraseMode(!eraseMode);
+    setEraseMode(!eraseMode);
+  };
   const handleColor = (event) => {
     setColor(event.target.value);
   };
@@ -16,7 +27,8 @@ const ScreenTestBoard = () => {
 
   return (
     <StyledWrapper>
-      <div className="color-picker">
+      <div className="brush-picker">
+        <span>브러쉬 설정</span>
         <input type="color" value={color} onChange={handleColor} />
         <select value={size} onChange={handleSize}>
           {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, key) => (
@@ -24,7 +36,23 @@ const ScreenTestBoard = () => {
           ))}
         </select>
       </div>
-      <Board color={color} size={size} />
+      <div className="eraser-picker">
+        <span>지우개 설정</span>
+        <select value={eraserSize} onChange={handleEraserSize}>
+          {[5, 10, 15, 20, 25, 30, 35].map((item, key) => (
+            <option key={key}>{item}</option>
+          ))}
+        </select>
+        <button onClick={handleEraseMode}>
+          {eraseMode ? '그리기' : '지우개'}
+        </button>
+      </div>
+      <Board
+        color={color}
+        size={size}
+        canvasRef={canvasRef}
+        eraserSize={eraserSize}
+      />
     </StyledWrapper>
   );
 };
@@ -34,4 +62,16 @@ export default ScreenTestBoard;
 const StyledWrapper = styled.div`
   height: 100%;
   width: 100%;
+
+  .brush-picker {
+    span {
+      color: white;
+    }
+  }
+
+  .eraser-picker {
+    span {
+      color: white;
+    }
+  }
 `;
